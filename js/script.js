71558997c949e8c383f4778319880e96576ef630
genius192x <mishalevchenko161@gmail.com>
2022-04-27 23:19:37 +0300

    main

--- js/script.js
+++ js/script.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
 		$('.header__burger, .header__items').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	$('.comments__slider').slick({
+		arrows: true,
+		dots: true,
+	});
+
+
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		// код для мобильных устройств
+		$('.welcome__card').click(function (event) {
+			if ($('.welcome__row').hasClass('one')) {
+				$('.welcome__card').not($(this)).removeClass('active');
+			}
+			$(this).toggleClass('active');
+
+		});
+	} else {
+		// код для обычных устройств
+	}
 });
+
+
